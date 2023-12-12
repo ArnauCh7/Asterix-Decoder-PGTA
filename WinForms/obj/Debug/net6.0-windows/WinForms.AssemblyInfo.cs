598bc916198398318875e7eaef5c39998c0e7b62
Arnau Chuecos <arnauchuecos@gmail.com>
2023-12-12 16:38:06 +0100

    iniciop3

--- WinForms/obj/Debug/net6.0-windows/WinForms.AssemblyInfo.cs
+++ WinForms/obj/Debug/net6.0-windows/WinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ac5a85334a6732f9adbbbe3983a378c555c0fd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
